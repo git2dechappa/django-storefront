0d304e3ea086fd588bd4b51261d71ef9e4006b80
oli <olibob229@gmail.com>
2022-04-12 23:38:26 +0100

    storefront

---
+++ playground/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def calculate():
+    x=1
+    y=2
+    return x
+
+
+# Create your views here.
+def say_hello(request):
+    x=calculate()
+    y=2
+#    return HttpResponse("Hello World")
+    return render(request, "hello.html",{"name": "Oli"})
